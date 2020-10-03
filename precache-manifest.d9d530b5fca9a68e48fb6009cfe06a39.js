b304016a2940715ebb76593e429640880ddb8645
Vitaly Kornatsky <vitaly.kornatsky@major-rp.ru>
2020-10-04 01:39:55 +0600

    Updates

---
+++ precache-manifest.d9d530b5fca9a68e48fb6009cfe06a39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6cca6bc76677d752c9093c1b4390e8d",
+    "url": "/tark-test/index.html"
+  },
+  {
+    "revision": "b4936ad826076dd2d6fa",
+    "url": "/tark-test/static/css/main.7e55aa99.chunk.css"
+  },
+  {
+    "revision": "1e0e905a3ab4cece05b2",
+    "url": "/tark-test/static/js/2.1cf91f2c.chunk.js"
+  },
+  {
+    "revision": "9f90cde4396c52b73cc0a6bcd8267b7b",
+    "url": "/tark-test/static/js/2.1cf91f2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4936ad826076dd2d6fa",
+    "url": "/tark-test/static/js/main.33f377e7.chunk.js"
+  },
+  {
+    "revision": "9d0298f63a5269f013ea",
+    "url": "/tark-test/static/js/runtime-main.aae2e38e.js"
+  }
+]);
